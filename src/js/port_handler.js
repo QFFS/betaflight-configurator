4f77cd1ebde0cd8f87c5038eb97c3bbdf66fa9f5
QFFS <lylyyhb@163.com>
2022-10-19 21:43:07 +0800

    Update port_handler.js
    Support Geehy APM32 DFU bootloader, So the flight controller which uses APM32 microcontroller can update firmware with betaflight configurator.

--- src/js/port_handler.js
+++ src/js/port_handler.js
@@ -8,6 +8,7 @@ const TCP_TIMEOUT = 2000;
 const usbDevices = { filters: [
     {'vendorId': 1155, 'productId': 57105}, // STM Device in DFU Mode || Digital Radio in USB mode
     {'vendorId': 10473, 'productId': 393},  // GD32 DFU Bootloader
+    {'vendorId': 12619, 'productId': 262},  // APM32 DFU Bootloader
 ] };
 
 const PortHandler = new function () {
